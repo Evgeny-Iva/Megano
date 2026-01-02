15da418a3fb0d7335df55c4e95fb590cfb7689af
Евгений Иванов <ivanov2012gen@yandex.ru>
2026-01-02 16:12:42 +0500

    Настройка urls

--- diploma-frontend/frontend/urls.py
+++ diploma-frontend/frontend/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="frontend/index.html")),
@@ -16,6 +17,6 @@ urlpatterns = [
     path('profile/', TemplateView.as_view(template_name="frontend/profile.html")),
     path('progress-payment/', TemplateView.as_view(template_name="frontend/progressPayment.html")),
     path('sale/', TemplateView.as_view(template_name="frontend/sale.html")),
-    path('sign-in/', TemplateView.as_view(template_name="frontend/signIn.html")),
+    path('sign-in/', views.SignInView.as_view(template_name="frontend/signIn.html")),
     path('sign-up/', TemplateView.as_view(template_name="frontend/signUp.html")),
 ]
