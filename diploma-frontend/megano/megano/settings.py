f53126343d196a1979ab81d25b9d5b3ffcdfb93e
Евгений Иванов <ivanov2012gen@yandex.ru>
2026-04-19 11:37:26 +0500

    Добавил README.md

--- diploma-frontend/megano/megano/settings.py
+++ diploma-frontend/megano/megano/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-46u-kxcru(3=v=w+k!e(c&94=&_wre%tx_)vuc*3k!!n)xr@+$'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
